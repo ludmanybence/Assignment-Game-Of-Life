454410fe29eca275e3bbf21bd2c78c26cbde22d3
Bence Ludmany <mokkants@gmail.com>
2017-09-10 19:23:37 +0200

    Complete solution

--- GOL.java
+++ GOL.java
@@ -1,18 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GOL{
 
     public static void main(String[] args){
         int gens = Integer.parseInt(args[0]);
         int line_nr = Integer.parseInt(args[1]);
-        List<String> temp = new ArrayList<String>();
+        ArrayList<String> temp = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < line_nr; i++){
             temp.add(in.nextLine());
         }
         in.close();
         String[] dish = new String[ temp.size() ];
-        temp.toArray( dish );
+        dish = temp.toArray(dish);
 
         String[] dish_old; // needed to check for termination of the game
 
@@ -30,19 +31,20 @@ public class GOL{
 
         // simulate generations
         for(int i= 0;i < gens;i++){
-            //System.out.println("Generation " + i + ":");
-            //print(dish);
+            System.out.println("Generation " + (i+1) + ":");
+            print(dish);
+            System.out.println();
             dish_old = dish;
             dish = life(dish);
             int terminated = compare (dish_old, dish);
             if (terminated == 1) {
                 print(dish);
-                //System.out.println("End of game, still life is reached.\n");
+                System.out.println("End of game, still life is reached.\n");
                 return;
             }
             if (terminated == 2) {
                 print(dish);
-                //System.out.println("End of game, all organisms are dead.\n");
+                System.out.println("End of game, all organisms are dead.\n");
                 return;
             }
 
@@ -67,17 +69,179 @@ public class GOL{
     }
 
     public static String[] life(String[] dish){
+
         String[] newGen= new String[dish.length];
 
-        //TODO: implement this function
+        //Initialize every row to empty string
+        for (int i = 0; i < newGen.length; i++) {
+            newGen[i] = "";
+        }
+
+        char alive = 'x';
+        char dead = 'o';
+
+        for (int i = 0; i < dish.length; i++) {
+            for (int j = 0; j < dish[0].length(); j++) {
+
+                boolean isAlive = (dish[i].charAt(j) == alive);
+                int neighboursAlive = 0;
+
+                Position cellPos = checkCoordinates(j,i, dish.length);
+
+                //Check neighbouring coordinates
+                switch(cellPos){
+
+                    case TOPLEFT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{1,0},
+                                new int[]{0,1},
+                                new int[]{1,1});
+                        break;
+                    case TOPRIGHT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j-1, 0},
+                                new int[]{j,1},
+                                new int[]{j-1,1});
+                        break;
+                    case BOTTOMLEFT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{0,i-1},
+                                new int[]{1, i},
+                                new int[]{1, i-1});
+                        break;
+                    case BOTTOMRIGHT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j-1,i},
+                                new int[]{j,i-1},
+                                new int[]{j-1,i-1});
+                        break;
+                    case RIGHT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j,i-1},
+                                new int[]{j,i+1},
+                                new int[]{j-1,i-1},
+                                new int[]{j-1,i+1},
+                                new int[]{j-1,i});
+                        break;
+                    case LEFT:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j,i-1},
+                                new int[]{j,i+1},
+                                new int[]{j+1,i-1},
+                                new int[]{j+1,i+1},
+                                new int[]{j+1,i});
+                        break;
+                    case TOP:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j,i+1},
+                                new int[]{j+1,i},
+                                new int[]{j-1,i},
+                                new int[]{j+1,i+1},
+                                new int[]{j-1,i+1});
+                        break;
+                    case BOTTOM:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j,i-1},
+                                new int[]{j+1,i},
+                                new int[]{j-1,i},
+                                new int[]{j+1,i-1},
+                                new int[]{j-1,i-1});
+                        break;
+                    case MIDDLE:
+                        neighboursAlive = checkNeighbours(dish,
+                                new int[]{j,i+1},
+                                new int[]{j,i-1},
+                                new int[]{j+1,i},
+                                new int[]{j-1,i},
+                                new int[]{j+1,i+1},
+                                new int[]{j-1,i+1},
+                                new int[]{j+1,i-1},
+                                new int[]{j-1,i-1});
+                        break;
+
+
+                }
+
+                //DEATH | Alive but lonely or too crowded
+                if(isAlive && (neighboursAlive < 2 || neighboursAlive > 3)){
+                    newGen[i]+=dead;
+                }
+                //BIRTH | Dead but with 3 neighbours
+                else if(!isAlive && neighboursAlive == 3){
+                    newGen[i]+=alive;
+                }
+                //STAY SAME | No change
+                else {
+                    newGen[i]+=dish[i].charAt(j);
+                }
+
+            }
+        }
 
         return newGen;
     }
 
+    //Return where in the grid a cell is (a corner, a side or the middle)
+    private static Position checkCoordinates(int x, int y, int arrayLength){
+
+        Position pos = Position.MIDDLE;
+        //arrayLength-1 because we compare to indices
+        arrayLength--;
+
+        if(x == 0){
+            if(y == 0){
+               pos = Position.TOPLEFT;
+            }
+            else if (y == arrayLength){
+                pos = Position.BOTTOMLEFT;
+            }
+            else{
+                pos = Position.LEFT;
+            }
+        }
+        else if(x==arrayLength){
+            if(y==0){
+                pos = Position.TOPRIGHT;
+            }
+            else if(y==arrayLength){
+                pos = Position.BOTTOMRIGHT;
+            }
+            else{
+                pos = Position.RIGHT;
+            }
+        }
+        else if(y==0){
+            pos = Position.TOP;
+        }
+        else if(y==arrayLength){
+            pos = Position.BOTTOM;
+        }
+
+        return pos;
+
+    }
+
+    //Count the number of alive neighbours around a cell
+    private static int checkNeighbours(String[] dish, int[]... coords){
+
+        char alive = 'x';
+        int neighboursAlive = 0;
+
+        for (int[] i : coords) {
+            if (dish[i[1]].charAt(i[0]) == alive) {
+                neighboursAlive++;
+            }
+        }
+
+        return neighboursAlive;
+
+    }
+
     public static void print(String[] dish){
-        for(String s: dish){
+        for(String s : dish){
             System.out.println(s);
         }
     }
 
 }
+
