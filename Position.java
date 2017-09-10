f06713bbf626ad76f36d3e34d236e0897c0ae571
Bence Ludmany <mokkants@gmail.com>
2017-09-10 19:22:20 +0200

    Working solution

---
+++ Position.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mokkants on 2017-09-10.
+ */
+public enum Position{
+
+    TOPLEFT,
+    TOPRIGHT,
+    BOTTOMLEFT,
+    BOTTOMRIGHT,
+    TOP,
+    LEFT,
+    RIGHT,
+    BOTTOM,
+    MIDDLE
+
+}
+
